1a91ef6e51df041e5e6beeabae2a1bee5a80de8d
Hafgun <hafpixels06@gmail.com>
2024-04-25 15:42:11 +0200

    added default_http_headers and default_http_params in make_request

--- ussd/screens/http_screen.py
+++ ussd/screens/http_screen.py
@@ -1,9 +1,11 @@
-from ussd.core import UssdHandlerAbstract
-from ussd.tasks import http_task
 import json
+
+from marshmallow import INCLUDE, Schema, fields, validate
+
+from ussd.core import UssdHandlerAbstract
 from ussd.graph import Link, Vertex
-from marshmallow import Schema, fields, validate, INCLUDE
-from ussd.screens.schema import UssdBaseScreenSchema, NextUssdScreenSchema
+from ussd.screens.schema import NextUssdScreenSchema, UssdBaseScreenSchema
+from ussd.tasks import http_task
 
 
 class HttpScreenConfSchema(Schema):
@@ -59,34 +61,40 @@ class HttpScreen(UssdHandlerAbstract):
 
         .. literalinclude:: .././ussd/tests/sample_screen_definition/valid_http_screen_conf.yml
     """
+
     screen_type = "http_screen"
     serializer = HttpScreenSchema
 
     def handle(self):
         http_request_conf = self.render_request_conf(
-            self.ussd_request.session,
-            self.screen_content['http_request']
+            self.ussd_request.session, self.screen_content["http_request"]
         )
 
-        if self.screen_content.get('synchronous', False):
+        if self.screen_content.get("synchronous", False):
             http_task.delay(request_conf=http_request_conf)
         else:
             self.make_request(
                 http_request_conf=http_request_conf,
-                response_session_key_save=self.screen_content['session_key'],
+                response_session_key_save=self.screen_content["session_key"],
                 session=self.ussd_request.session,
-                logger=self.logger
+                default_http_headers=self.ussd_request.default_http_headers or {},
+                default_http_params=self.ussd_request.default_http_params or {},
+                logger=self.logger,
             )
         return self.route_options()
 
     def show_ussd_content(self, **kwargs):
-        results = "http_screen\n{}".format(json.dumps(self.screen_content['http_request'],
-                                                   indent=2, sort_keys=True))
+        results = "http_screen\n{}".format(
+            json.dumps(self.screen_content["http_request"], indent=2, sort_keys=True)
+        )
         results = results.replace('"', "'")
         return results
 
     def get_next_screens(self):
         return [
-            Link(Vertex(self.handler), Vertex(self.screen_content['next_screen']),
-                 self.screen_content['session_key'])
+            Link(
+                Vertex(self.handler),
+                Vertex(self.screen_content["next_screen"]),
+                self.screen_content["session_key"],
+            )
         ]
